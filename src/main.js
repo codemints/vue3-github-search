53ba6b21084930e1767779cf2f5e73c02fa886ab
Daniel LittleThunder <lunaco5280@gmail.com>
2022-04-21 13:11:53 -0600

    added fontawesome icon imports to main.js so they would be included in build

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@scss/main.scss'
+import "@ico/font-awesome/scss/fontawesome.scss"
+import "@ico/font-awesome/scss/solid.scss"
+import "@ico/font-awesome/scss/brands.scss"
+import "@ico/font-awesome/scss/regular.scss"
 import { createPinia } from 'pinia'
 
 createApp(App).use(createPinia()).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -16,5 +16,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './'),
       '~': path.resolve(__dirname, './')
     }
-  }
+  },
 })
